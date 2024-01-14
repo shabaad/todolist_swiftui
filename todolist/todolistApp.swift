8190fa3c3cf0e54d7f7e8cac2668a1462e08ccd4
shabaadh <139437754+shabaadh@users.noreply.github.com>
2024-01-14 11:49:53 +0530

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Admin on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
